54c67dc05f8c1384adafe9f303d3eb803ea21768
zhang jian <afzhangjian@163.com>
2016-12-06 10:09:24 +0800

    Initial Commit

---
+++ SameFunctionModules/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SameFunctionModules
+//
+//  Created by jian zhang on 2016/12/6.
+//  Copyright © 2016年 jian zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
